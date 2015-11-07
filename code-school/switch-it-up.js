5c0c76fd53c396de3622aa8c22361f189c355d2e
Kayode Alayode <ksamuel.chris@me.com>
2015-11-07 10:57:04 -0500

    added code to prevent fall through

--- code-school/switch-it-up.js
+++ code-school/switch-it-up.js
@@ -2,10 +2,16 @@ function sansTemple(direction) {
   var happensNext;
     switch (direction) {
       case "right hallway":
-         this.happensNext = 'You find a door, it is locked. You head back from where you came.'
+         this.happensNext = 'You find a door, it is locked. You head back from where you came.' ;break;
       case 'left hallway':
-         this.happensNext = 'You find a door, it is locked. You head back from where you came.'
-}
+         this.happensNext = 'You find a door, it is locked. You head back from where you came.' ;break;
+      case 'lower tunnel':
+          this.happensNext = 'You find a door, it is locked. You head back from where you came.' ;break;
+      case 'hidden door':
+          this.happensNext = 'You go through the door into a room. A small,very dark, room, that smells of dust and sulfur....' ;break;
+      default :
+      this.happensNext = 'You stand there , gaping not moving . Aiedra wonders, are you alive?' ;break ;
+       }
   return happensNext;
 }
 sansTemple();
