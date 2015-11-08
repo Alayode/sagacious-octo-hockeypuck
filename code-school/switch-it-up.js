df876c22575bfb88188fd06ec30fc0fc9a6c5201
Kayode Alayode <ksamuel.chris@me.com>
2015-11-08 12:05:27 -0500

    grouped cases to one event

--- code-school/switch-it-up.js
+++ code-school/switch-it-up.js
@@ -2,11 +2,8 @@ function sansTemple(direction) {
   var happensNext;
     switch (direction) {
       case "right hallway":
-         this.happensNext = 'You find a door, it is locked. You head back from where you came.' ;break;
       case 'left hallway':
-         this.happensNext = 'You find a door, it is locked. You head back from where you came.' ;break;
       case 'lower tunnel':
-          this.happensNext = 'You find a door, it is locked. You head back from where you came.' ;break;
       case 'hidden door':
           this.happensNext = 'You go through the door into a room. A small,very dark, room, that smells of dust and sulfur....' ;break;
       default :
