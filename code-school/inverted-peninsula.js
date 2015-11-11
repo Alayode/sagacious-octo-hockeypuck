c4a1f5a3ac0c4d7c5a16ab62ff25bd4ef3ce2a62
azuramei <admin@MacBook-Pro.local>
2015-11-11 17:16:54 -0500

    created InvertedPeninsula Object

--- code-school/inverted-peninsula.js
+++ code-school/inverted-peninsula.js
@@ -3,4 +3,23 @@
 //Filename : Inverted Peninsula
 
 
-var InvertedPeninsula = function(){}
+var InvertedPeninsula = function(){
+  this.inhabitants = [
+    {
+      name: "Choshizen",
+      race:  "Elvaan"
+    },
+    {
+      name: "Tessius",
+      race:  "Hume"
+    }
+  ];
+  // Add an extra humans method property to the inhabitants array to return all Humans
+  this.inhabitants.humans = function(){
+    /* returns all Human inhabitatans*/
+  };
+  // create a new invertedPeninsula instance
+  var invertedPeninsula = new InvertedPeninsula();
+  // Log the name of each invertedPeninsula inhabitants
+
+}
