d6e42164bce7ea7694235d3a4686fa20e6865da8
Chris Samuel <ksamuel.chris@icloud.com>
2015-11-11 10:22:47 -0500

    added the multiplier variable

--- code-school/thirty-three.js
+++ code-school/thirty-three.js
@@ -11,3 +11,13 @@ function starter(){
 }
 
 console.log(starter); //[Function: starter]
+
+function multiplier(y){
+  var x, z;
+  x = 10;
+  z = y * x;
+  return z;
+ }
+
+
+console.log(multiplier);//[Function:multiplier]
