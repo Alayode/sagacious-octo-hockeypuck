be516e68fdb29bbf70810ff7cf21b595cfcb9996
Chris Kayode Samuel <ksamuel.chris@icloud.com>
2015-11-11 18:13:30 -0500

    created IIFE and console log to access the objects in inhabitants ear

--- code-school/inverted-peninsula.js
+++ code-school/inverted-peninsula.js
@@ -3,8 +3,8 @@
 //Filename : Inverted Peninsula
 
 
-var InvertedPeninsula = function(){
-  this.inhabitants = [
+(function(){
+  inhabitants = [
     {
       name: "Choshizen",
       race:  "Elvaan"
@@ -14,12 +14,19 @@ var InvertedPeninsula = function(){
       race:  "Hume"
     }
   ];
-  // Add an extra humans method property to the inhabitants array to return all Humans
-  this.inhabitants.humans = function(){
-    /* returns all Human inhabitatans*/
-  };
-  // create a new invertedPeninsula instance
-  var invertedPeninsula = new InvertedPeninsula();
-  // Log the name of each invertedPeninsula inhabitants
+//   // Add an extra humans method property to the inhabitants array to return all Humans
+// //  this.inhabitants.humans = function(){
+//     /* returns all Human inhabitatans*/
+//   };
+//   // create a new invertedPeninsula instance
+//   //var invertedPeninsula = new InvertedPeninsula();
+//
+//   // Log the name of each invertedPeninsula inhabitants
+//         for(var i = 0, o = invertedPeninsula.inhabitants; i < o.length; i++){
+//     console.log(invertedPeninsula.inhabitants[i].name);
+  // }
+})();
 
-}
+var ppl = inhabitants;
+console.log(ppl);
+// var g = new InvertedPeninsula();
