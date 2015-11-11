4bc049901ff4c7356ffd924f32000d2831bbf165
Chris Kayode Samuel <ksamuel.chris@icloud.com>
2015-11-11 18:22:31 -0500

    showed how NOT to add new instances within the constructor

--- code-school/inverted-peninsula.js
+++ code-school/inverted-peninsula.js
@@ -15,20 +15,23 @@ var InvertedPeninsula = function(){
     }
   ];
   console.log("My People ",inhabitants)
-//   // Add an extra humans method property to the inhabitants array to return all Humans
-// //  this.inhabitants.humans = function(){
-//     /* returns all Human inhabitatans*/
-//   };
-//   // create a new invertedPeninsula instance
-//   //var invertedPeninsula = new InvertedPeninsula();
-//
-//   // Log the name of each invertedPeninsula inhabitants
-//         for(var i = 0, o = invertedPeninsula.inhabitants; i < o.length; i++){
-//     console.log(invertedPeninsula.inhabitants[i].name);
-  // }
+
+  // Add an extra humans method property to the inhabitants array to return all Humans
+  this.inhabitants.humans = function(){
+    /* returns all Human inhabitatans*/
+    console.log(" My Humans!!!")
+  };
+  // create a new invertedPeninsula instance
+  var invertedPeninsula = new InvertedPeninsula(); //causes a infinite callback BAD BAD BAD
+
+  // Log the name of each invertedPeninsula inhabitants
+        for(var i = 0, o = invertedPeninsula.inhabitants; i < o.length; i++){
+            console.log(invertedPeninsula.inhabitants[i].name);
+  }
 };
 
 InvertedPeninsula();
+
 
 // var ppl = inhabitants;
 // console.log(ppl);
