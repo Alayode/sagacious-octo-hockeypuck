a50f65f5ecbcd2b2322c4b84400010ffe4441418
Chris Kayode Samuel <ksamuel.chris@icloud.com>
2015-11-17 16:36:43 -0500

    building a Date object and explaining the use of the unary operator

---
+++ code-school/JS-Best-Practices/pendant-performance/numerical-time-data.js
@@ -0,0 +1,22 @@
+
+
+// To accurately generate numberical time data, we can manipulate, we'll first examine
+//the JavaScript Date object.
+
+
+var now = +new Date();
+var later = +new Date();
+console.log(now); // this new Date iobject immediately captures, the current date and time.
+//Tue Nov 17 2015 16:30:12 GMT-0500 (EST)
+
+// Now lets place an unary operator in front of our Date Object variable
+// asks for the specific value in milliseconds
+
+
+//the difference between the two values will be the amount
+// of time that passed betweenthe creation of both variable
+var elapsedTime = endTime - rightNow;
+console.log(elapsedTime)
+
+console.log(now);
+// 1447795812900
