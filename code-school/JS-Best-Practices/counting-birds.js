f95a6f64e86184f4ce380b8b4cf5def341a5b5a8
Chris Samuel <ksamuel.chris@icloud.com>
2015-11-22 17:40:24 -0500

    created variables for application

---
+++ code-school/JS-Best-Practices/counting-birds.js
@@ -0,0 +1,30 @@
+
+
+//Chris Samuel
+/*
+How many SyntaxBird objects inhabit the dunes?
+Modify the conditional in the loop so that it
+will progress through the duneInhabitants
+array and properly increment the count variable.
+*/
+
+
+//create a constructor and name it Bird
+function Bird(){}
+
+//create a constructor function name it DatatypeBird(){}
+function DatatypeBird(){}
+
+//create a constructor function name it SyntaxBird
+function SyntaxBird(){}
+
+
+DatatypeBird.protoype = Object.create(Bird.protoype);
+SyntaxBird.prototype = Object.create(Bird.prototype);
+
+
+var TryCatchTaster = function() {},
+    SwitchSnagger  = function() {},
+    TernaryTracker = function() {};
+
+var NumberNommer = function() {};
