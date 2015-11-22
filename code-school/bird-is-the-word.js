2675ad43917932516324e8085737a3aa05a2ecb2
Chris Samuel <ksamuel.chris@icloud.com>
2015-11-22 12:04:32 -0500

    added a simple console.log to the DataTypeBird constructor function

--- code-school/bird-is-the-word.js
+++ code-school/bird-is-the-word.js
@@ -3,3 +3,8 @@
 function Bird() {
   Bird.prototype = console.log("Im the bird");
 };
+
+//declare a new constructor function call it DataType Bird(){};
+function DataTypeBird(){
+   DataTypeBird.prototype = console.log("Im the DataTypeBird")
+};
