7611445bd58ea0d2c806234d3c6861f9e4f74a22
Chris Samuel <ksamuel.chris@icloud.com>
2015-11-22 17:14:11 -0500

    within a new function we will create a new instance of Bird

--- code-school/bird-is-the-word.js
+++ code-school/bird-is-the-word.js
@@ -15,3 +15,22 @@ DataTypeBird();
 // on the DataTypeBird prototype create a new object and assign it
 // to DataTypeBird.prototype.
 DataTypeBird.prototype = Object.create(Bird.prototype);
+
+
+// create an anonymous function that takes no parameters and assign it to  a new variable
+// constructor call it NumberNommer
+var NumberNommer = function(){};
+
+
+//On the NumberNommer prototype create a new object and pass it the DataTypeBird.prototype
+NumberNommer.prototype = Object.create(DataTypeBird.prototype);
+
+// MyNumberNommer is now a new instance of bird
+// this will now only give
+
+
+
+console.log(NumberNommer); // Returns a function
+
+// Returns a function with a empty object
+console.log(NumberNommer.prototype);
