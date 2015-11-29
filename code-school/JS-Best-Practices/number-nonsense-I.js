384295790bb660328309e5a920f53c445b80e282
Chris Samuel <ksamuel.chris@icloud.com>
2015-11-29 15:21:27 -0500

    Created new page and set the variables for the birdFeeder object

---
+++ code-school/JS-Best-Practices/number-nonsense-I.js
@@ -0,0 +1,16 @@
+//Number Nonsense I
+//Chris Samuel
+//Github : Alayode
+//email: ksamuel.chris@icloud.com
+
+
+var birdFeeder = {
+  totalFeed: 4,
+  getFeed: function(feedRequested) {
+    var feedRequested = 0;
+    console.log(feedRequested ,'inside ');
+  }
+}
+
+birdFeeder.getFeed();
+console.log(birdFeeder.totalFeed);
