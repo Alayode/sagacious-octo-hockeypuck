19b0c7f76bef43263d50b1d290dfe98718f28766
Chris Samuel <ksamuel.chris@icloud.com>
2015-12-02 12:28:28 -0500

    created the comments necessary for laying out private and public objects

--- code-school/privatizing-properties.js
+++ code-school/privatizing-properties.js
@@ -12,12 +12,28 @@ Github: alayode
 */
 
 var ARMORY = (function(){
-  var weaponList =  [ * list of weapons object * ];
-  var armorList  =  [ *list of armor objects * ];
+  var weaponList =  [ 'Excalibur','Ice Brand','Ragnarok','Save the Queen' ];
+  var armorList  =  [ 'Rainbow Cloak','Doublet','Lizard Jerkin','Chain Mail' ];
 
-  var removeWeapon = function(...){};
-  var replaceWeapon = function(...){};
-  var removeArmor = function(...){};
-  var replaceArmor = function(...){};
-  
+  var removeWeapon = function(){
+    //revmove weapon from the armory
+  };
+  var replaceWeapon = function(){
+    //add a weapon to the weaponList
+  };
+  var removeArmor = function(){
+    //remove a weapons from the armorList
+  };
+  var replaceArmor = function(){
+    //add armor to the armorList
+  };
+
+  return{
+    makeWeaponRequest: function(){
+    //create a object that would be accessible to the global scope.
+    };
+    makeArmorRequest: function(){
+      // create a object that also be available to the global scope once the function is called.
+    };
+  };
 })();
