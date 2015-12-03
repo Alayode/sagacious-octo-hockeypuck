31e1fc861ec377e6c399cfa09790ebdef823266b
Chris Samuel <ksamuel.chris@icloud.com>
2015-12-03 11:34:50 -0500

    add local variables and pass it a function that will return the local variable protecting it from the global scope

--- code-school/build-module.js
+++ code-school/build-module.js
@@ -6,4 +6,8 @@
 //apply the CAVEOSCLARITY to the bottom of the closure.
 CAVESOFCLARITY = (function(){
 
+  // inside our module create a local variable and assign it an empty string
+    var sandScript = '';
+    
+
 })(CAVEOSCLARITY);
