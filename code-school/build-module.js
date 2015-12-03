11589dd33d296edbb124f906aa78db2c5f86d529
Chris Samuel <ksamuel.chris@icloud.com>
2015-12-03 11:31:58 -0500

    create a module namespace

---
+++ code-school/build-module.js
@@ -0,0 +1,7 @@
+//first build a CAVESOFCLARITY namespace
+//assign to it an empty function expression that is immediately invoked
+//don't use var for augmentation
+
+//Next pass the caves variable as a parameter and then
+//apply the CAVEOSCLARITY to the bottom of the closure.
+CAVESOFCLARITY = function(){}; 
