985eebdd165eb6fd1b7388edc91617bab721e76a
Chris Samuel <ksamuel.chris@icloud.com>
2015-12-04 09:16:38 -0500

    created directions for next question

--- code-school/JS-RoadTrip-3/HauntedHickoryHouse.js
+++ code-school/JS-RoadTrip-3/HauntedHickoryHouse.js
@@ -82,3 +82,17 @@
 // Test Your Code!!!!
 // Call the function and ensure that your parameters are
 // being passed in the right order, and only by variable name.
+
+
+
+
+
+// Displaying Functions contents
+// Periodically, the devs at the Haunted Hickory
+// House need to be reminded of their formula.
+// They would like to view the contents of the function,
+// rather than executing it.
+//
+// Write one line of code to alert the contents of the
+// variable that stores the function to the screen.
+// Here is the fearGenerated function expression assignment for reference:
