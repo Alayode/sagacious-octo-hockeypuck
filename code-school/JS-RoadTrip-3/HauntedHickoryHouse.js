91d233a976a49b8a5effffeb0d86bc59a31819d4
Chris Samuel <ksamuel.chris@icloud.com>
2015-12-04 09:14:39 -0500

    Next Question

--- code-school/JS-RoadTrip-3/HauntedHickoryHouse.js
+++ code-school/JS-RoadTrip-3/HauntedHickoryHouse.js
@@ -66,19 +66,19 @@
 
 // If the math is troubling, try to work backwards
 // from an ideal number in the accepted range of 100 to 400.
-
-var people = 5;
-var rain = 6;
-var sharks = 6;
-
-
-var fearGenerated = function(numPeeps, rainInInches, numSharks) {
-  var rainFear = numPeeps * rainInInches;
-  var sharkFear = numSharks * numSharks * numSharks;
-  var totalFear = sharkFear + rainFear;
-  return totalFear;
-};
-
+//
+// var people = 5;
+// var rain = 6;
+// var sharks = 6;
+//
+//
+// var fearGenerated = function(numPeeps, rainInInches, numSharks) {
+//   var rainFear = numPeeps * rainInInches;
+//   var sharkFear = numSharks * numSharks * numSharks;
+//   var totalFear = sharkFear + rainFear;
+//   return totalFear;
+// };
+//
 // Test Your Code!!!!
 // Call the function and ensure that your parameters are
 // being passed in the right order, and only by variable name.
