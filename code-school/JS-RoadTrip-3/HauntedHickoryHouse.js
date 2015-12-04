cbd5089682dfc65a9eb9cfa059616e2c28b6f4ab
Chris Samuel <ksamuel.chris@icloud.com>
2015-12-04 08:53:58 -0500

    created statement of purpose

---
+++ code-school/JS-RoadTrip-3/HauntedHickoryHouse.js
@@ -0,0 +1,12 @@
+//Chris Samuel
+//Github: Alayode
+//2015-3-12
+
+// Code School: Changing Declarations to Expressions
+
+// Developers for the Forest of Function Expressions
+// Theme Park have created a function declaration named forestFright,
+// but they’ve decided not to keep the function in memory.
+// Convert the function from a named function
+// declaration to an anonymous function
+// expression and assign it to a variable called runAway.
