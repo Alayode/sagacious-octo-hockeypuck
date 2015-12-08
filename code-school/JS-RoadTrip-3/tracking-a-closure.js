6da20aa025c6898a082ce3c2794911c7e19079de
Chris Samuel <ksamuel.chris@icloud.com>
2015-12-08 10:28:03 -0500

    Tracking a closure 1: added directions

---
+++ code-school/JS-RoadTrip-3/tracking-a-closure.js
@@ -0,0 +1,17 @@
+
+//Chris Samuel
+//ksamuel.chris@icloud.com
+
+
+//Tracing a Closure 1
+
+// Examine the code below(i.e., manually trace it), in your head determine the final
+// value of the result variable, and alert the value as a numer using one line of code.
+
+    // You must give only 1 number literal as the argument to alert.
+    // Do not do any calculations. So, for example, don't use *. (Definitely do some math
+    // in your head though!)
+    // Don't pass in a string.
+    // The number must be a whole number. No Decimal points.
+
+    
